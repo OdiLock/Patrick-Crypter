8a52cfdd4ddd0697ad55d53eac2a8ac512be7f73
OdiLock <112762400+OdiLock@users.noreply.github.com>
2022-09-03 15:52:50 +0300

    Add files via upload

---
+++ Patrick Crypter Builder/Patrick Jane Crypter Builder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Patrick_Jane_Crypter_Builder
+{
+	static class Program
+	{
+		/// <summary>
+		/// Uygulamanın ana girdi noktası.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+
+            
+
+            Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+
+
+
+        }
+
+    }
+}
